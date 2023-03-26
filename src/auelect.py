54cadccea46b90a956fad3205c2d0ead4fc0adca
azuki774 <azuki774s@gmail.com>
2023-03-26 22:54:01 +0900

    add insert db

--- src/auelect.py
+++ src/auelect.py
@@ -3,6 +3,7 @@ import logging
 import glob
 from bs4 import BeautifulSoup
 import datetime
+import db
 
 t_delta = datetime.timedelta(hours=9)
 JST = datetime.timezone(t_delta, "JST")
@@ -21,26 +22,27 @@ logger.addHandler(h)
 
 
 def _extract_html(f):
-    html = open(f, "r")
+    with open(f) as html:
+        ret = []
+        soup = BeautifulSoup(html, "html.parser")
 
-    ret = []
-    soup = BeautifulSoup(html, "html.parser")
+        table = soup.findAll("table")[0]
+        rows = table.findAll("tr")
+        for row in rows:
+            ts = row.findAll("td")
+            if ts == []:
+                continue
+            day_text = ts[0].get_text()  # ex. "    11 (土)"
+            consume_text = ts[1].get_text()  # ex. "8.50 kWh"
+            price_text = ts[2].get_text()  # ex. "   111 円"
+            day, consume, price = [
+                day_text.split(" ")[-2],
+                consume_text.split(" ")[0],
+                price_text.split(" ")[-2],
+            ]
 
-    table = soup.findAll("table")[0]
-    rows = table.findAll("tr")
-    for row in rows:
-        ts = row.findAll("td")
-        if ts == []:
-            continue
-        day_text = ts[0].get_text()  # ex. "    11 (土)"
-        consume_text = ts[1].get_text()  # ex. "8.50 kWh"
-        price_text = ts[2].get_text()  # ex. "   111 円"
-        day, consume, price = [
-            day_text.split(" ")[-2],
-            consume_text.split(" ")[0],
-            price_text.split(" ")[-2],
-        ]
-        ret.append([day, consume, price])  # # ex. [11, 8.50, 111]
+            # # ex. ['11', 8.50, 111]
+            ret.append([day, float(consume), int(price)])
 
     logger.info("extract html data complete: %s", f)
     return ret
@@ -48,9 +50,9 @@ def _extract_html(f):
 
 def _proc_file(file):
     logger.info("proc file: %s", file)
-    _extract_html(file)
-
+    data = _extract_html(file)
     yyyymm = now.strftime("%Y%m")
+    db.insert_auelect(data, yyyymm)
     return 0
 
 
@@ -63,6 +65,8 @@ def auelect_main():
 
     files = glob.glob("/data/*")
     for f in files:
+        if f[0] == "_":  # prefix が _ のファイルは処理済と見なす
+            continue
         _proc_file(f)
 
     logger.info("auelect end")
---
+++ src/db.py
@@ -0,0 +1,50 @@
+import mysql.connector
+from pythonjsonlogger import jsonlogger
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+h = logging.StreamHandler()
+h.setLevel(logging.DEBUG)
+json_fmt = jsonlogger.JsonFormatter(
+    fmt="%(asctime)s %(levelname)s %(filename)s %(lineno)s %(message)s",
+    json_ensure_ascii=False,
+)
+h.setFormatter(json_fmt)
+logger.addHandler(h)
+
+
+def _connect_db():
+    try:
+        cnx = mysql.connector.connect(
+            user=os.environ.get("DB_USER", "root"),
+            password=os.environ.get("DB_PASS", "password"),
+            host=os.environ.get("DB_HOST", "localhost"),
+            database=os.environ.get("DB_NAME", "billmanager"),
+        )
+        logger.info("connect DB")
+        return cnx
+    except Exception as e:
+        logger.error("failed to connect DB: %s", e)
+        os.exit(1)
+
+
+def insert_auelect(data, yyyymm):
+    cnx = _connect_db()
+    cursor = cnx.cursor()
+    add_data = (
+        "INSERT INTO elect_consumption"
+        "(record_date, total_consumption, day_consumption, night_consumption) "
+        "VALUES ({}, {}, {}, {})"
+    )
+
+    for d in data:
+        print(d)
+        sql = add_data.format('"' + yyyymm + d[0] + '"', int(float(d[1]) * 1000), 0, 0)
+        print(sql)
+        cursor.execute(sql)
+
+    cnx.commit()
+    cursor.close()
+    cnx.close()
